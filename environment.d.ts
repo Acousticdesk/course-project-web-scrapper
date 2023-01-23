810b9f6602cfbcd8c6b487ca167067d0651af1a3
Andrii Kicha <akicha@star.global>
2023-01-23 18:23:52 +0200

    scrap multiple pages

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       WEBSITE: string;
+      NUM_PAGES_TO_SCRAP: string;
     }
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,10 @@ import fs from "fs";
 dotenv.config();
 
 import puppeteer from "puppeteer";
+import chalk from "chalk";
 
 import { queryLinks } from "links/query";
+import { ScrapperLink } from "links/interfaces";
 
 (async () => {
   const browser = await puppeteer.launch({
@@ -12,14 +14,20 @@ import { queryLinks } from "links/query";
   });
   const page = await browser.newPage();
 
-  await page.goto(process.env.WEBSITE);
+  let links: ScrapperLink[] = [];
 
-  await page.waitForSelector("#search-results");
+  for (let i = 1; i <= Number(process.env.NUM_PAGES_TO_SCRAP); i++) {
+    await page.goto(`${process.env.WEBSITE}?page=${i}`);
+    await page.waitForSelector("#search-results");
 
-  fs.writeFileSync(
-    "links.json",
-    JSON.stringify(await queryLinks(page), null, 2)
-  );
+    const linksOnPage = await queryLinks(page);
+
+    if (linksOnPage) {
+      links = links.concat(linksOnPage);
+    }
+  }
+
+  fs.writeFileSync("links.json", JSON.stringify(links, null, 2));
 
   await browser.close();
 })();
