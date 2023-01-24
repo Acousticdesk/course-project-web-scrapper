be87daa5e50db994d61e6e10ef24b40707e53394
Andrii Kicha <akicha@star.global>
2023-01-24 17:25:17 +0200

    track for installment payments

--- src/real-estate-item/query.ts
+++ src/real-estate-item/query.ts
@@ -16,9 +16,11 @@ export async function queryRealEstateItem(
       ".BuildingContacts-developer-name span"
     )?.textContent;
 
-    const attributes = [...body.querySelectorAll(".BuildingAttributes-info")];
+    const attributeElements = [
+      ...body.querySelectorAll(".BuildingAttributes-info"),
+    ];
 
-    const attributesOfInterest = attributes.reduce<RealEstateItemAttributes>(
+    const attributes = attributeElements.reduce<RealEstateItemAttributes>(
       (acc, attribute) => {
         const realEstateAttributesMap = {
           клас: "class",
@@ -39,9 +41,12 @@ export async function queryRealEstateItem(
         if (currentAttribute && currentAttribute in realEstateAttributesMap) {
           const currentAttributeTyped =
             currentAttribute as (typeof realEstateAttributesMap)[keyof typeof realEstateAttributesMap];
-          acc[currentAttributeTyped] = attribute.querySelector(
-            ".BuildingAttributes-value"
-          )?.textContent;
+
+          acc[
+            realEstateAttributesMap[
+              currentAttributeTyped as keyof typeof realEstateAttributesMap
+            ]
+          ] = attribute.querySelector(".BuildingAttributes-value")?.textContent;
         }
 
         return acc;
@@ -49,10 +54,49 @@ export async function queryRealEstateItem(
       {} as RealEstateItemAttributes
     );
 
+    const financialsElement = body.querySelector("#credit_installments");
+
+    let financials: {
+      installmentPlan?: boolean;
+      installmentPlanTerm?: number | null;
+    } = {};
+
+    if (financialsElement) {
+      let installmentElement = null;
+      let hasInstallmentPlan = false;
+
+      [...financialsElement.querySelectorAll(".UICardLink-title")].forEach(
+        (element) => {
+          if (element.textContent?.toLowerCase()?.includes("розтермінування")) {
+            installmentElement = element;
+            hasInstallmentPlan = true;
+          }
+        }
+      );
+
+      if (installmentElement) {
+        const installmentPlanDetailsRaw = (installmentElement as Element)
+          .closest(".UICardLink-content")
+          ?.querySelector(".UICardLink-description")
+          ?.textContent?.trim();
+
+        // на 1 рік, на 2 роки ...
+        const installmentPlanDetails = installmentPlanDetailsRaw
+          ? /на\s(\d)\sр/g.exec(installmentPlanDetailsRaw)?.[1]
+          : null;
+
+        financials.installmentPlan = true;
+        financials.installmentPlanTerm = installmentPlanDetails
+          ? window.parseInt(installmentPlanDetails, 10)
+          : null;
+      }
+    }
+
     return {
       residence,
       developer,
-      attributes: attributesOfInterest,
+      attributes,
+      financials,
     };
   });
 }
