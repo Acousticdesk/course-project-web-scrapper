6d9747c5bb6922d9bddfd7962f56d27d4553b78d
Andrii Kicha <akicha@star.global>
2023-03-08 20:39:15 +0100

    add average price (assess) to a real estate item

---
+++ src/mappers/price-statistics.ts
@@ -0,0 +1,45 @@
+import realEstateDataset from "../../real-estate-1678304145096.json";
+import fs from "fs";
+
+const priceStatisticsPerDistrict = {
+  Дарницький: 42027,
+  Дніпровський: 34392,
+  Деснянський: 30949,
+  Голосіївський: 52536,
+  "Солом'янський": 37885,
+  Святошинський: 47299,
+  Оболонський: 89888,
+  Подільський: 53927,
+  Печерський: 136750,
+  Шевченківський: 72634,
+};
+
+class PriceStatisticsMapper {
+  static calculateExpectedPrice(dataset: typeof realEstateDataset) {
+    return dataset.map((realEstateItem) => {
+      let subjectDistrict;
+
+      for (const district in priceStatisticsPerDistrict) {
+        if (realEstateItem.district.match(district)) {
+          subjectDistrict = district;
+          break;
+        }
+      }
+
+      return {
+        ...realEstateItem,
+        // @ts-ignore
+        assess: priceStatisticsPerDistrict[subjectDistrict] || null,
+      };
+    });
+  }
+}
+
+fs.writeFileSync(
+  `real-estate-${Date.now()}.json`,
+  JSON.stringify(
+    PriceStatisticsMapper.calculateExpectedPrice(realEstateDataset),
+    null,
+    2
+  )
+);
